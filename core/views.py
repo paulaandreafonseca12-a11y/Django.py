6075b6ffb43907131e06e5077dfb0ebaec0b46e5
paulaandreafonseca12-a11y <paulaandreafonseca12@gmail.com>
2026-03-19 14:14:55 -0500

    fix: agregar modulos y formularios

--- core/views.py
+++ core/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.contrib import messages  # type: ignore
+
+
+from django.shortcuts import render,redirect, get_object_or_404 # type: ignore
+
+import servicios
+from servicios.forms import serviciosEditarForm, serviciosForm
 
 def inicio(request):
     nombre = "Paula"
--- servicios/models.py
+++ servicios/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from django.db import models # type: ignore
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser # type: ignore
+
+
+class Servicios(models.Model):
+    
+    codigo_servicios = models.CharField(max_length=8, unique=True, verbose_name='Código de servicios')
+    nombre = models.CharField(max_length=150, verbose_name='nombre')
+    precio = models.CharField(max_length=10, verbose_name='Precio')
+    duracion = models.CharField(max_length=20, verbose_name='Duración')
+    cita_servicios = models.ForeignKey('servicios', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='cita_servicios')
+
+    class Meta:
+        verbose_name = 'Servicio' #singular 
+        verbose_name_plural = 'Servicios'# plural 
+        
+    def __str__(self):
+        return self.nombre
+
